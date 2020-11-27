d40e4af72880b9c22a35f3bf0c6dd0469f5f5f0d
VardonH <hugo.vardon@etud.univ-jfc.fr>
2020-11-27 09:15:27 +0100

    commit exo3 1

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,28 @@
+package geco;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class LoginGeneratorTest {
+
+    private LoginGenerator logGen;
+    private LoginService logServ;
+
+    @Before
+    public void setUp() throws Exception {
+        final String [] c = new String[3];
+        c[0] = "PDUP";
+        c[1] = "VHUG";
+        c[2] = "TAGA";
+        logServ = new LoginService(c);
+        logGen = new LoginGenerator(logServ);
+
+    }
+
+    @Test
+    public void testGenerateLoginForNomAndPrenom() {
+        assertEquals("PDUP1",logGen.generateLoginForNomAndPrenom("Dupond", "Paul"));
+        assertEquals("JDUR",logGen.generateLoginForNomAndPrenom("Durand", "Jacques"));
+    }
+}
